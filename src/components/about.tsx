db46eeeec7cc9c4a57fd9ac8e1f6c65e90279983
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-09-21 08:09:46 +0000

    make the image big

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -11,7 +11,7 @@ export function About() {
     <section id="about" className="py-20 md:py-32">
       <div className="container mx-auto px-4 md:px-6">
         <div className="grid md:grid-cols-2 gap-10 md:gap-20 items-center">
-          <div className="relative w-full aspect-square max-w-md mx-auto">
+          <div className="relative w-full aspect-square mx-auto">
             <Image
               alt="About Me Image"
               className="rounded-lg object-cover"
