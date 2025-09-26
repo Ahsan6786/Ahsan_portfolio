229649f27c8b85b8c88862553ddbc86ee7d80b9b
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-09-26 19:57:08 +0000

    now thats not good, i want full netflix tyoe animations and instead of A

--- src/components/intro-animation.tsx
+++ src/components/intro-animation.tsx
@@ -6,8 +6,7 @@ export function IntroAnimation() {
   return (
     <div className="intro-container">
       <div id="container">
-        <div id="netflixintro">A</div>
-        <div id="brush"></div>
+        <div id="netflixintro">Ahsan</div>
       </div>
     </div>
   );
