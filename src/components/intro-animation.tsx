6d48fda4cf2955e44b7639603dbee6eab57193a7
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-09-26 19:59:27 +0000

    i want golden animations

--- src/components/intro-animation.tsx
+++ src/components/intro-animation.tsx
@@ -3,11 +3,11 @@
 import React from 'react';
 
 const colors = [
-  '#ff0000', '#ff3d00', '#ff7a00', '#ffb800', '#fff500', 
-  '#c5ff00', '#88ff00', '#4bff00', '#0eff00', '#00ff3d', 
-  '#00ff7a', '#00ffb8', '#00fff5', '#00c5ff', '#0088ff',
-  '#004bff', '#000eff', '#3d00ff', '#7a00ff', '#b800ff',
-  '#f500ff', '#ff00c5', '#ff0088', '#ff004b'
+  '#FFD700', '#FFDE59', '#FFE68A', '#FFECB3', '#FFF2D9',
+  '#FFFFFF', '#FFF2D9', '#FFECB3', '#FFE68A', '#FFDE59',
+  '#FFD700', '#FFC400', '#FFBF00', '#FFB347', '#FFA500',
+  '#FFB347', '#FFBF00', '#FFC400', '#FFD700', '#FFDE59',
+  '#FFE68A', '#FFECB3', '#FFF2D9', '#FFFFFF'
 ];
 
 const barStyles = colors.map((color, i) => {
