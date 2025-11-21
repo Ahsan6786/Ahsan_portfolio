91649f2cd431eb93e40a5db3eccc94dcff9a555c
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-11-21 20:54:25 +0000

    bahi project block ka ize chota karo mobile aur desktop dono view me, th

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -274,7 +274,7 @@ export default function ProjectsPage() {
                 These are some of my recent projects. Check out my GitHub for more. Tap any card to see more details.
               </p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {projects.map((project, index) => (
                 <ProjectCard key={index} project={project} onInfoClick={() => setSelectedProject(project)} />
               ))}
