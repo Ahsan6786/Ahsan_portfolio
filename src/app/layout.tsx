d310df01cef5d174807c19a9167e6ca66f4fea82
46_Ahsan Khan <ahsan768fg@gmail.com>
2025-11-21 15:33:01 +0000

    <!--Start of Tawk.to Script--> <script type="text/javascript"> var Tawk_

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -103,6 +103,20 @@ export default function RootLayout({
             <ScrollToTopButton />
           </ThemeProvider>
         </LanguageProvider>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{ __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/669285f2c32ff219649a97d3/1i2mf7055';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          ` }}
+        />
       </body>
     </html>
   );
